b64b5dc1aee4f8b3ca1c69d8f4b4d4b92a24460b
AnyAng8 <anykarin2804@gmail.com>
2025-11-09 00:25:54 +0100

    no entiendo nada

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
  // Aquí tu código
+const lista = document.getElementById("lista")
+const agregar = document.getElementById("agregar")
+
+agregar.addEventListener("click", function() {
+
+})
+
+const lugares = prompt (" Consulta un lugar")
+
+const li = document.createElement ("li")
+li.textContent = lugares
+lista.appendChild(li)
